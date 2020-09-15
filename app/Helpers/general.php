bf117375e912c87e64fa65e75e87e37418658f46
Mohamed <mabskh@gmail.com>
2020-09-15 19:07:25 +0300

    =''

---
+++ app/Helpers/general.php
@@ -0,0 +1,15 @@
+<?php
+
+function getFolder()
+{
+    return app()->getLocale() == 'ar' ? 'css-rtl' : 'css';
+}
+
+
+function uploadImage($folder,$image)  // take Two Arguments  1. folder,  2. image
+{
+    $image->store('/', $folder);
+    $fileName = $image->hashName();
+    // $path = '/images' . $folder . $fileName;
+    return $fileName;
+}
